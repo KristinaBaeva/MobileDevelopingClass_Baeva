b88d944aa5b5e10fc41e8dc390120aede9a1f008
Kristina Baeva <melart785@gmail.com>
2025-03-25 00:06:50 +0300

    Лабораторная 4. Решила сделать по-другому

---
+++ lab4/models/weather_card.dart
@@ -0,0 +1,15 @@
+class WeatherCard {
+  final String temperature;
+  final String city;
+  final String country;
+  final String weatherType;
+  final String assetPath;
+
+  WeatherCard({
+    required this.temperature,
+    required this.city,
+    required this.country,
+    required this.weatherType,
+    required this.assetPath,
+  });
+}
